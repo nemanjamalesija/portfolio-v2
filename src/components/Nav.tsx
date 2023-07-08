787f1a08cad22d4e0b32b7b716f25ca0d90fc814
nemanjamalesija <malesija13@gmail.com>
2023-07-08 23:18:10 +0200

    Clean nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -10,20 +10,15 @@ const Nav = () => {
     >
       <div className='logo'>
         <h3 className='nav__logo text-5xl inline-block justify-self-start cursor-pointer'>
-          Synergy
+          N.M.
         </h3>
       </div>
       <ul className='nav__list flex justify-between items-center text-2xl cursor-pointer tracking-widest'>
-        <li className='nav__list--item'>Artists</li>
         <li className='nav__list--item'>About</li>
-        <li className='nav__list--item'>News</li>
+        <li className='nav__list--item'>Projects</li>
         <li className='nav__list--item'>Contact</li>
       </ul>
-      <div className='nav__search justify-self-end inline-block'>
-        <p className='nav__search-p text-2xl cursor-pointer tracking-widest'>
-          Search
-        </p>
-      </div>
+      <div className='nav__search justify-self-end inline-block'></div>
     </nav>
   );
 };
